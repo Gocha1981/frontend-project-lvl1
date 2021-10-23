803aef20c0972e462b77a1aa807de80fcbcfc102
Gocha <gochahome@gmail.com>
2021-10-23 17:53:51 +0300

    fix progression4

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,15 +2,15 @@ import readlineSync from 'readline-sync';
 const getrandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 export const progression = (start, step, length) => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ');
+    console.log(`Welcome to the Brain Games!`);
+    const name = readlineSync.question(`May I have your name? `);
     console.log(`Hello, ${name}!`);
-    console.log('What number is missing in the progression?');
+    console.log(`What number is missing in the progression?`);
 
     for (let j = 0; j < 3; j += 1) {
         const question = [];
         const numbers = getrandomNumber(0, 100);
-        const numbersLength = (getrandomNumber(8,10));
+        const numbersLength = (getrandomNumber(10,10));
         const stepLength = getrandomNumber(1, 100);
         const hiddenNumber = getrandomNumber(0, numbersLength);
 
